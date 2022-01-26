bc5792a672c09aa00e70f44f7ccc9597c9f4ba2e
Galina-Goncharova <galinagoncharova1112@gmail.com>
2022-01-26 09:41:51 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery")
+
+
+const listImg = galleryItems
+    .map(({ preview, original, description}) => `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`)
+    .join("");
+console.log(listImg);
+
+galleryRef.innerHTML = listImg;
+
+function handlerClick(event) {
+    event.preventDefault();
+
+    const source = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+`)
+
+    instance.show()
+}
+
+galleryRef.addEventListener("click", handlerClick)
+
+
+
+
+
+
+
+
